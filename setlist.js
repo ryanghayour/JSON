b5e5b10e63f9beea36825d7da8172ee584d2db98
ryanghayour <56457626+ryanghayour@users.noreply.github.com>
2020-03-31 00:46:11 -0400

    Add files via upload

---
+++ setlist.js
@@ -0,0 +1,53 @@
+var setlist = {
+	"songs": [{
+		"name": "Ocean Man",
+		"artist": "Ween",
+		"genre": "Alt Rock",
+		"year": "2000",
+	}, {
+		"name": "Because I got high",
+		"artist": "Shaggy",
+		"genre": "Hip Hop",
+		"year": "2000",
+	}, {
+		"name": "Fruit Salad",
+		"artist": "Wiggles",
+		"genre": "Television",
+		"year": "2000",
+	}, {
+		"name": "You Say Run",
+		"artist": "Yuki Hiyashi",
+		"genre": "Television",
+		"year": "2016",
+	}, {
+		"name": "Two Princes",
+		"artist": "Spin Doctors",
+		"genre": "Rock",
+		"year": "1991",
+	}, {
+		"name": "1 Thot 2 Thot",
+		"artist": "Yung Gravy",
+		"genre": "Rap",
+		"year": "2018",
+	}, {
+		"name": "Your Man",
+		"artist": "Josh Turner",
+		"genre": "Country",
+		"year": "2005",
+	}, {
+		"name": "Goofy Goober Rock",
+		"artist": "Tom Rothrock",
+		"genre": "Rock",
+		"year": "2004",
+	}, {
+		"name": "Can't Stop Won't Stop",
+		"artist": "Mighty & High",
+		"genre": "Rap",
+		"year": "2016",
+	}, {
+		"name": "Tubthumping",
+		"artist": "Chumbawamba",
+		"genre": "Rock",
+		"year": "1997",
+	}, ]
+}
